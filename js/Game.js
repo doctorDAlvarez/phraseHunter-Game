cadb63fc8e5ed61836ae50629c36ed97415b503f
Diego Alvarez <dendrit83@protonmail.ch>
2021-04-14 17:09:37 -0300

    hard work debuggin and exceeds

--- js/Game.js
+++ js/Game.js
@@ -2,88 +2,91 @@
  * Project 4 - OOP Game App
  * Game.js */
 
- class Game {
-   constructor() {
-     this.missed = 0;
-     this.phrases = ["JavaScript Rocks",
-                     "Do Not Repeat Yourself",
-                     "Python or JavaScript",
-                     "Animal Planet",
-                     "Stucked in unit four"]
-                     .map( phrase => new Phrase(phrase));
-     this.activePhrase = null;
-   }
 
-   startGame() {
-     document.getElementById("overlay").style.display = "none";
-     this.activePhrase = this.getRandomPhrase();
-     this.activePhrase.addPhraseToDisplay();
-   }
 
-   getRandomPhrase() {
-      const rand_index = Math.floor(Math.random() * this.phrases.length);
-      return this.phrases[rand_index];
-    }
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      "JavaScript Rocks",
+      "Do Not Repeat Yourself",
+      "Python or JavaScript",
+      "Animal Planet",
+      "Stucked in unit four"
+    ].map(phrase => new Phrase(phrase));
+    this.activePhrase = null;
+  }
 
-   handleInteraction( key_button ) {
-     key_button.disabled = true;
-     if ( this.activePhrase.checkLetter( key_button.textContent ) ) {
-        key_button.classList.add("chosen");
-        this.activePhrase.showMatchedLetter( key_button.textContent );
-        if ( this.checkForWin() ) {
-          this.gameOver();
-        }
-     } else {
-        key_button.classList.add("wrong");
-        this.removeLife();
+  startGame() {
+    this.resetGame();
+    document.getElementById("overlay").style.display = "none";
+    this.activePhrase = this.getRandomPhrase();
+    this.activePhrase.addPhraseToDisplay();
+  }
+
+  getRandomPhrase() {
+    const rand_index = Math.floor(Math.random() * this.phrases.length);
+    return this.phrases[rand_index];
+  }
+
+  handleInteraction(interaction) {
+    interaction.disabled = true;
+    if (this.activePhrase.checkLetter(interaction.textContent)) {
+      interaction.classList.add("chosen");
+      this.activePhrase.showMatchedLetter(interaction.textContent);
+      if (this.checkForWin()) {
+        this.gameOver();
       }
-   }
+    } else {
+      interaction.classList.add("wrong");
+      this.removeLife();
+    }
+  }
+  removeLife() {
+    const score = document.querySelectorAll(".tries");
+    if (this.missed < 5) {
+      score[this.missed].firstElementChild.src = "images/lostHeart.png";
+      this.missed += 1;
+      if (this.missed === 5) {
+        this.gameOver();
+      }
+    }
+  }
 
-   removeLife() {
-     const score = document.querySelectorAll(".tries");
-     score[this.missed].firstElementChild.src = "images/lostHeart.png";
-     this.missed += 1;
-     if (this.missed === 5) {
-       this.gameOver();
-     }
+  checkForWin() {
+    const hide_letters = document.querySelectorAll(".hide");
+    if (hide_letters.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-   }
+  gameOver() {
+    document.getElementById("overlay").style.display = "flex";
+    if (this.checkForWin()) {
+      document.getElementById("overlay").className = "win";
+      document.querySelector("#game-over-message").textContent =
+        "You win, Congratulations!";
+    } else {
+      document.getElementById("overlay").className = "lose";
+      document.querySelector("#game-over-message").textContent =
+        "Oops try harder!";
 
-   checkForWin() {
-     const hide_letters = document.querySelectorAll(".hide");
-     if ( hide_letters.length === 0 ) {
-       return true;
-     } else {
-       return false;
-     }
-   }
-
-   gameOver() {
-     //displays original overlay.
-     document.getElementById("overlay").style.display = "flex";
-     if ( this.checkForWin() ) {
-        document.getElementById("overlay").className = "win";
-        document.querySelector("#game-over-message").textContent = "You win, Congratulations!";
-        this.resetGame();
-     } else {
-       document.getElementById("overlay").className = "lose";
-       document.querySelector("#game-over-message").textContent = "Oops try harder!";
-       this.resetGame();
-       }
-   }
-
-   resetGame() {
-     const placeHolders = document.querySelector("ul");
-     const score = document.querySelectorAll(".tries");
-     const keys = document.querySelectorAll(".chosen ,.wrong, .key");
-     this.missed = 0;
-     placeHolders.innerHTML = "";
-     score.forEach( elem => elem.firstElementChild.src = "images/liveHeart.png");
-     keys.forEach( key => {
-          key.disabled = false;
-          key.className = "key";
-     });
-
-   }
-
+    }
+  }
+  resetGame() {
+    const placeHolders = document.querySelector("ul");
+    const keys = document.querySelectorAll(".chosen ,.wrong, .key");
+    const score = document.querySelectorAll(".tries");
+    this.missed = 0;
+    placeHolders.innerHTML = "";
+    score.forEach(
+      elem => (elem.firstElementChild.src = "images/liveHeart.png")
+    );
+    keys.forEach(key => {
+      key.disabled = false;
+      key.className = "key";
+    });
+  }
 }
--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@
 const new_game = new Game();
 const start_button = document.querySelector("#btn__reset");
 const qwerty = document.querySelector("#qwerty");
-const title = document.querySelector("h2.title");
+const key_buttons = document.querySelectorAll(".key");
+const arr_keys = Array.from(key_buttons);
 
 start_button.addEventListener("click", event => {
   new_game.startGame();
@@ -14,4 +15,13 @@ qwerty.addEventListener("click", event => {
   if (event.target.tagName == "BUTTON") {
     new_game.handleInteraction( event.target );
   }
+});
+
+document.addEventListener("keydown", event => {
+    button = arr_keys.filter( button => button.textContent === event.key);
+    if ( !(button[0] === undefined) ) {
+      if ( !button[0].disabled ){
+          new_game.handleInteraction( button[0] );
+      }
+    }
 });
